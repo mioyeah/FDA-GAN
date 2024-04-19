e5c0aa1d023d5a855a9d458ab32bbe42007bc2f7
mioyeah <79745767+mioyeah@users.noreply.github.com>
2024-04-19 17:33:34 +0800

    Add files via upload

---
+++ FDAGAN/tools/background_inpaintors/__init__.py
@@ -0,0 +1,12 @@
+
+def build_background_inpaintors(name, *args, **kwargs):
+
+    if name == "mmedit_inpainting":
+        from .mmedit_inpaintors import SuperResolutionInpaintors
+
+        inpaintors = SuperResolutionInpaintors(*args, **kwargs)
+
+    else:
+        raise ValueError(f"{name} is not valid background inpaintors.")
+
+    return inpaintors
